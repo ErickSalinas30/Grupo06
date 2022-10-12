848c56ff5ec74e8aa277acfcb080944d5dfff9c3
usuario <usuario@172.16.219.194>
2022-10-12 12:35:39 -0500

    Public class clases

---
+++ src/main/java/com/mycompany/nueva_clase2/Nueva_clase2.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.nueva_clase2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Nueva_clase2 {
+
+    public static void main(String[] args) {
+        
+        var auto1 = new Auto();
+        
+        auto1.marca= "Chevrolet";
+        auto1.nombrePropietario= "Erick Salinas";
+        auto1.placa= "BBU-786";
+        
+        System.out.println(auto1.mostrarInfo()+" el pais de origen del auto es "+auto1.getPaisMarca());
+        
+        var auto2 = new Auto();
+        
+        auto2.marca= "Toyota";
+        auto2.nombrePropietario= "Gabriel Salas";
+        auto2.placa= "FFU-786";
+       
+        System.out.println(auto2.mostrarInfo()+" el pais de origen del auto es "+auto2.getPaisMarca());
+         
+        var auto3 = new Auto();
+        
+        auto3.marca="Jetour";
+        auto3.nombrePropietario= "Luis Miguel";
+        auto3.placa= "FFU-111";
+       
+        System.out.println(auto3.mostrarInfo()+" el pais de origen del auto es "+auto3.getPaisMarca());
+        
+        var auto4 = new Auto();
+        
+        auto4.marca= "Mercedes";
+        auto4.nombrePropietario= "Julio Jaramillo";
+        auto4.placa= "FFU-566";
+      
+        System.out.println(auto4.mostrarInfo()+" el pais de origen del auto es "+auto4.getPaisMarca());
+        
+        var auto5 = new Auto();
+        
+        auto5.marca= "Kia";
+        auto5.nombrePropietario= "Gabriela Salinas";
+        auto5.placa= "PPP-266";
+      
+        System.out.println(auto5.mostrarInfo()+" el pais de origen del auto es "+auto5.getPaisMarca());
+        
+    }
+}
